4bd3d6cf9556e6e301987c3c91cea0f32cb27c0c
Jake <jake.horton94@hotmail.co.uk>
2020-10-06 17:21:25 +0100

    Linked 'news' button to developer blog

--- src/components/navbar/nav.component.jsx
+++ src/components/navbar/nav.component.jsx
@@ -8,7 +8,7 @@ const Nav = () => (
         <Link to='/'><img alt='Kingfisher Logo' src='https://i.imgur.com/8HtEWZl.jpg'/></Link>
         <ul>
             <li><Link to='/cv'>CV</Link></li>
-            <li><Link to='/reading'>Reading</Link></li>
+            <li><a href='https://speckyboy.com/'>News</a></li>
             <li><Link to='/calculator'>Calculator</Link></li>
         </ul>
     </nav>
